29fe5e16c6993e1735a2b99640d8457231e8c519
leoliang <leoliang@futunn.com>
2020-04-05 17:13:07 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -37,6 +37,10 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereViewCount($value)
  * @mixin \Eloquent
+ * @property-read \App\Models\Category $category
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic recentReplied()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic withOrder($order)
  */
 class Topic extends Model
 {
@@ -51,5 +55,32 @@ class Topic extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
     }
 }
